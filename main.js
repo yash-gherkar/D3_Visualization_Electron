85c2612c9fbaec467a4ddbd128ba3800ad78ab13
yash-gherkar <yashgherkar@gmail.com>
2025-07-04 01:44:40 +0200

    Electron integeration

---
+++ main.js
@@ -0,0 +1,22 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'), // Optional, for secure context
+      contextIsolation: true, // Recommended for security
+      nodeIntegration: false  // Recommended for security
+    }
+  });
+
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
